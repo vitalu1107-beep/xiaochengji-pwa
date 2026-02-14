53d423a4d0faa07d3693056f457594dd8910ee6a
vitalu1107-beep <vitalu1107@gmail.com>
2026-02-15 03:04:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * - Update strategy: skipWaiting + clientsClaim
  */
 
-const VERSION = "v2026-02-15-04";
+const VERSION = "v2026-02-15-05";
 const CACHE_NAME = `smallwins-cache-${VERSION}`;
 const RUNTIME_CACHE = `smallwins-runtime-${VERSION}`;
 
